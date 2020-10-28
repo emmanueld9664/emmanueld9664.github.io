c7e16f0ae28dec62e2df42d70e514222f5239a32
emmanueld9664 <73564442+emmanueld9664@users.noreply.github.com>
2020-10-27 20:03:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Emmanuel Dorta"
+let userName = "emmanueld9664";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
